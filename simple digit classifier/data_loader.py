1551adcfc544a0555b045879278b0a02cbeaf9b1
lazy-toad <vraj722006@gmail.com>
2026-08-07 14:27:46 +0530

    load_data

--- simple digit classifier/data_loader.py
+++ simple digit classifier/data_loader.py
@@ -1,12 +1,20 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("./archive/mnist_train.csv")
-data = np.array(data)
-labels = data[:,0]
-features = data[:,1:]
-features = features/255.0
 
-print(features.shape)
-print(features.min())
-print(features.max())
+def load_data():
+    data = pd.read_csv("./archive/mnist_train.csv")
+    data = np.array(data)
+    labels = data[:,0]
+    features = data[:,1:]
+    features = features/255.0
+
+    #print(features.shape)
+    #print(features.min())
+    #print(features.max())
+
+
+    encoded_labels = np.zeros((60000,10))
+    encoded_labels[np.arange(60000),labels] = 1
+
+    return encoded_labels, features
